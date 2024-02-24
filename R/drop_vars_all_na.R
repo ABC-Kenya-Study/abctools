3dfb17d81cb8956ac25866d9c27c7d3fe95e34a1
cmzuidema <czuidema@uw.edu>
2024-02-23 16:39:16 -0800

    add supporting info for drop_vars_all_na function.

--- R/drop_vars_all_na.R
+++ R/drop_vars_all_na.R
@@ -1,3 +1,13 @@
+#' Drop columns or variables that are all NA
+#'
+#' @param x A dataframe.
+#'
+#' @return A dataframe.
+#' @export
+#'
+#' @examples
+#' (x <- data.frame(a = 1:4, b = rep(NA, 4), c = 5:8, d = c(NA, 'B', 'C', 'D')))
+#' drop_vars_all_na(x)
 drop_vars_all_na <- function(x){
   x |> dplyr::select(tidyselect::where(~any(!is.na(.x))))
 }

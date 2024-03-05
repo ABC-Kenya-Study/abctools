641d5f754cd6b3e0b0c1722e1b11a64c2e778d60
cmzuidema <czuidema@uw.edu>
2024-03-04 18:42:06 -0800

    rename id_yes_no_vars to id_yesno_vars.

--- R/id_yesno_vars.R
+++ R/id_yesno_vars.R
@@ -13,8 +13,8 @@
 #'
 #' @examples
 #' (x <- data.frame(var1 = c('Yes', 'yes', 'no', 'No'), var2 = 1:4, var3 = c('yes', rep(NA, 3))))
-#' id_yes_no_vars(x)
-id_yes_no_vars <- function(x){
+#' id_yesno_vars(x)
+id_yesno_vars <- function(x){
   lapply(x, stringr::str_detect, '^Yes$|^yes$|^YES$|^Y$|^y$|^No$|^no$|^NO$|^N$|^n$') |>
     lapply(all, na.rm = TRUE) |>
     purrr::keep(isTRUE) |>

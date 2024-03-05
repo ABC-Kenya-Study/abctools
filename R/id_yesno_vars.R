a91ab9ad0209fec47ec93a137fce7f0d6f7f6c05
cmzuidema <czuidema@uw.edu>
2024-03-04 20:02:25 -0800

    drop NA variables from input dataframe.

--- R/id_yesno_vars.R
+++ R/id_yesno_vars.R
@@ -15,6 +15,7 @@
 #' (x <- data.frame(var1 = c('Yes', 'yes', 'no', 'No'), var2 = 1:4, var3 = c('yes', rep(NA, 3))))
 #' id_yesno_vars(x)
 id_yesno_vars <- function(x){
+  x <- abctools::drop_vars_all_na(x)
   lapply(x, stringr::str_detect, '^Yes$|^yes$|^YES$|^Y$|^y$|^No$|^no$|^NO$|^N$|^n$') |>
     lapply(all, na.rm = TRUE) |>
     purrr::keep(isTRUE) |>
